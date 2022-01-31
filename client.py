caf5c53e8fe13a5b4aee4e060fa4fa4dc1dce053
oscardahblerg <oscar.dahlberg1@gmail.com>
2022-01-31 17:00:27 +0100

    Fixed bug when checking for no nodes

--- client.py
+++ client.py
@@ -53,21 +53,28 @@ def get_id(name):
     url = "http://localhost:8080/pm/api/nodes?session={}&name={}".format(session, name)
     headers = {'Content-Type': 'application/json'}
     response = requests.get(url, headers=headers)
+
     data = response.json()["entity"]
+    if response.json()["message"] != "Success":
+        return 0, response.json()["message"]
+
+    if len(data) < 1:
+        return 0, "no nodes found"
+
     nId = data[0]["id"]
 
-    print(response.json()["code"])
-    return nId, response.json()["entity"][0]["msg"]
+    print(response.json()["message"])
+    return nId, response.json()["message"]
 
 
 # Skapa ett assignment mellan en child nod och en parent nod
 def make_assignment(node_name, group_name):
     node_id, msg = get_id(node_name)
-    if not msg == "Success":
+    if msg != "Success":
         return "Error getting node_id" + msg
 
     group_id, msg = get_id(group_name)
-    if not msg == "Success":
+    if msg != "Success":
         return "Error getting group_id" + msg
 
     url = "http://localhost:8080/pm/api/assignments?session=" + format(session)
@@ -80,3 +87,23 @@ def make_assignment(node_name, group_name):
     response = requests.post(url, headers=headers, data=body)
     print(response.json()["message"])
     return response.json()["message"]
+
+
+def get_associations(node_name):
+    msg, node_id = get_id(node_name)
+    if msg != "Success":
+        return "Error getting node_id" + msg
+
+    url = "http://localhost:8080/pm/api/associations?" \
+          "targetId={}?session={}".format(node_id, session)
+    headers = {'Content-Type': 'application/json'}
+
+    response = requests.get(url, headers=headers)
+    print(response.json()["message"])
+    return response.json()["entity"], response.json()["message"]
+
+
+# Kollar om username har en nod som heter username_seller
+def is_seller(username):
+    msg, seller_id = get_id(username + "_seller")
+    return msg
