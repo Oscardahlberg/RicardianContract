d6b348535e10e47b0e3581e4453c75c4bf53545d
oscardahblerg <oscar.dahlberg1@gmail.com>
2022-01-31 15:45:53 +0100

    Added make_assignment function and edited create_id

--- client.py
+++ client.py
@@ -4,61 +4,78 @@ import json
 import requests
 
 PORT = 49000
-#Skapa en session och byt ut till din här!
-session = "E159D282E2014CDBA40F680E659395B9"
-
-def post(msg):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((socket.gethostname(), PORT))
-    s.send(jpysocket.jpyencode(msg))
-    s.close()
+# Skapa en session och byt ut till din här!
+session = "43D39E76E4F94E0BAD39409E97521F83"
 
 
-def get(msg):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((socket.gethostname(), PORT))
-    s.send(jpysocket.jpyencode(msg))
-
-    msg = s.recv(1024)
-    msg = jpysocket.jpydecode(msg)
-    print("Received: ", msg)
-    s.close()
-    
-#Skapa en session!
+# Skapa en session!
 def sessions():
     url = "http://localhost:8080/pm/api/sessions"
     headers = {'Content-Type': 'application/json'}
-    body = json.dumps({"username" : "super", "password" : "super"})
-    response = requests.post(url, headers = headers, data = body)
+    body = json.dumps({"username": "super", "password": "super"})
+    response = requests.post(url, headers=headers, data=body)
     print(response.json()["entity"])
     return response.json()["entity"]
 
-#Skapa nod!
-def createNode(name, type, desc, prop1, prop2):
-    url ="http://localhost:8080/pm/api/nodes?session={}".format(session)
-    headers = {'Content-Type': 'application/json'}
-    body = json.dumps({
-  "name": "{}".format(name),
-  "type": "{}".format(type),
-  "description": "{}".format(desc),
-  "properties": [
-    {
-      "key": "{}".format(prop1),
-      "value": "{}".format(prop2)
-    }
-  ]
-}
-)
-    print(url)
-    response = requests.post(url, headers = headers, data = body)
-    print(response.json())
 
-#Hämta nodens ID baserat på nodens namn
-def getId(name):
+# Skapa nod!
+# Den sista variabeln är frivilig, ex: create_node("namn", "U", "desc...")
+def create_node(name, type, desc, *argv):
+    url = "http://localhost:8080/pm/api/nodes?session={}".format(session)
+    headers = {'Content-Type': 'application/json'}
+    if argv:
+        body = json.dumps({
+            "name": name,
+            "type": type,
+            "description": desc,
+            "properties": [
+                {
+                    "key": argv[0],
+                    "value": argv[1]
+                }
+            ]
+        })
+    else:
+        body = json.dumps({
+            "name": name,
+            "type": type,
+            "description": desc,
+            "properties": []
+        })
+
+    response = requests.post(url, headers=headers, data=body)
+    print(response.json())
+    return response.json()["message"]
+
+
+# Hämta nodens ID baserat på nodens namn
+def get_id(name):
     url = "http://localhost:8080/pm/api/nodes?session={}&name={}".format(session, name)
     headers = {'Content-Type': 'application/json'}
-    response = requests.get(url, headers = headers)
+    response = requests.get(url, headers=headers)
     data = response.json()["entity"]
     nId = data[0]["id"]
-    print(nId)
-    return nId
+
+    print(response.json()["code"])
+    return nId, response.json()["entity"][0]["msg"]
+
+
+# Skapa ett assignment mellan en child nod och en parent nod
+def make_assignment(node_name, group_name):
+    node_id, msg = get_id(node_name)
+    if not msg == "Success":
+        return "Error getting node_id" + msg
+
+    group_id, msg = get_id(group_name)
+    if not msg == "Success":
+        return "Error getting node_id" + msg
+
+    url = "http://localhost:8080/pm/api/assignments?session=" + format(session)
+    body = json.dumps({
+        "childId": node_id,
+        "parentId": group_id
+    })
+    headers = {'Content-Type': 'application/json'}
+
+    response = requests.post(url, headers=headers, data=body)
+    return print(response.json()["message"])
